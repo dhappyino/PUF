626db74d0d2122344c15ed80b8bb2ebbe751ba13
dhappyino <54205417+dhappyino@users.noreply.github.com>
2022-08-28 18:27:02 +0800

    list

--- PUF.py
+++ PUF.py
@@ -5,21 +5,20 @@ array = []
 
 password = input('使用者輸入密碼:')
 print('使用者密碼:', password)
-hash_password = hashlib.sha256(password.encode(
-    "utf-8")).hexdigest()  # 用sha256對password進行加密
+hash_password = hashlib.sha256(password.encode("utf-8")).hexdigest()  # 用sha256對password進行加密
 print("十六進位:", hash_password)
 number = int(hash_password, 16)
 print("十進位:", number)  # 十六轉十
 decimalValue = number
 binaryString = ""
-value = decimalValue
-while value != 0:
-    binaryString = str(value % 2) + binaryString
-    value //= 2
-print("二進位: ", binaryString)
+while decimalValue != 0:
+    binaryString = str(decimalValue % 2) + binaryString
+    decimalValue //= 2
+print("二進位: ", list(binaryString))
+print(list(binaryString)[1])
+print(len(str(binaryString)))  # 1024個
 print()
 array = [random.randint(10, 99) for x in range(1024)]
-print(array)
 print(len(array))  # 1024個
 print("--------------------第一組--------------------")
 print(array[0:256])
